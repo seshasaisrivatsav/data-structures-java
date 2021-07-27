88d2ca3b5ef009dc46e2e7198b178e79c8337d14
seshasaisrivatsav <seshasaisrivatsav@gmail.com>
2021-07-27 12:51:53 -0400

    updates

---
+++ env.sh
@@ -0,0 +1,7 @@
+## Be sure to change M2_HOME when running this on a different machine
+export M2_HOME=/Users/skuchibhotla/learn/apache-maven-3.8.1
+export M2=$M2_HOME/bin
+export MAVEN_OPTS=-Xms1024m
+export PATH=$M2:$PATH
+git config --global user.name "seshasaisrivatsav"
+git config --global user.email "seshasaisrivatsav@gmail.com"
