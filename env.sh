696c718ce61e7e246a32e4192bd106a965ad4fe5
Sesha Sai Srivatsav Kuchibhotla <seshasaisrivatsav@gmail.com>
2021-08-02 12:07:54 -0400

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,5 +3,5 @@ export M2_HOME=/Users/skuchibhotla/learn/apache-maven-3.8.1
 export M2=$M2_HOME/bin
 export MAVEN_OPTS=-Xms1024m
 export PATH=$M2:$PATH
-git config --global user.name "seshasaisrivatsav"
-git config --global user.email "seshasaisrivatsav@gmail.com"
+git config user.name "seshasaisrivatsav"
+git config user.email "seshasaisrivatsav@gmail.com"
