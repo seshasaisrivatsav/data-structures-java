10116c39f9bc11987a3735739bc3a39134ada41c
seshasaisrivatsav <seshasaisrivatsav@gmail.com>
2023-02-22 12:20:39 -0600

    array rotate, clock and anti clock

--- src/main/java/arraysAndStrings/RotateArray.java
+++ src/main/java/arraysAndStrings/RotateArray.java
@@ -2,42 +2,67 @@ package arraysAndStrings;
 
 import java.util.Arrays;
 
+/**
+ * Right Rotate = Clockwise rotation
+ * <a href="https://leetcode.com/problems/rotate-array/description/">...</a>
+ * Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+ *	Input: nums = [1,2,3,4,5,6,7], k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ */
 public class RotateArray {
 	
 	public static void main(String[] args) {
-		int[] arr1 = {1,2,3,4,5,6,7,8};
-		int rTimes=5;
-
 		int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-		rotateArray_clockwise_reversal(arr, 3);
+		rotateArray_clockwise(arr, 3);
 		System.out.println(Arrays.toString(arr));
 
+		int[] arr1 = {1,2,3,4,5,6,7,8};
+		rotateArray_clockwise(arr1, 5);
+		System.out.println(Arrays.toString(arr1));
+
 		int[] arr2 = {1};
-		rotateArray_clockwise_reversal(arr2, 0);
+		rotateArray_clockwise(arr2, 0);
+		System.out.println(Arrays.toString(arr2));
 
 		int[] arr4 = {1, 2};
-		rotateArray_clockwise_reversal(arr4, 2);
+		rotateArray_clockwise(arr4, 2);
+		System.out.println(Arrays.toString(arr4));
+
+		int[] arr5 = {1,2, 3, 4, 5};
+		rotateArray_clockwise(arr5, 1);
+		System.out.println(Arrays.toString(arr5));
+
+		int[] arr6 = {1,2, 3, 4, 5};
+		rotateArray_antiClockwise(arr6, 2);
+		System.out.println(Arrays.toString(arr6));
 	}
 
 
 	/**
-	 * Approach 1: Rotate array of length n, r times
+	 * Approach: Rotate array of length n, r times
 	 * 		     Array = (n-r) array + (r) array
 	 * 		     Reverse (n-r) array, Reverse (r) array
 	 * 		     Reverse the above
-	 *
 	 * 		     Eg: 1, 2, 3, 4, 5, 6, 7  n=7, r=3
+	 * 		     To rotate this thrice 1, 2, 3, 4, 5, 6, 7
+	 * 		     1st rotation: 7, 1, 2, 3, 4, 5, 6
+	 * 		     2nd rotation: 6, 7, 1, 2, 3, 4, 5
+	 * 		     3rd rotation: 5, 6, 7, 1, 2, 3, 4
 	 * 		     Reverse (7-3 = 4) array, reverse 3 array
 	 * 		     (1, 2, 3, 4) , (5, 6, 7)
 	 * 		     (4, 3, 2, 1), (7, 6, 5)
 	 * 		     Reverse the above
 	 * 		     5, 6, 7, 1, 2, 3, 4, 5
 	 * Time Complexity: O(N)
-	 * Space Complexity
+	 * Space Complexity: O(1) in place
 	 * @param arr int[]
 	 * @param times int
 	 */
-	public static void rotateArray_clockwise_reversal(int[] arr, int times) {
+	public static void rotateArray_clockwise(int[] arr, int times) {
 		if (times >= arr.length) {
 			times = times % arr.length; // coz rotating array of length 5, 5 times, 10 times is same
 		}
@@ -57,4 +82,34 @@ public class RotateArray {
 		}
 	}
 
+	/**
+	 * Approach: Rotate array of length n, r times
+	 * 		     Step 1: Array = (n) array + (n-r) array
+	 * 		     Step 2: Reverse (n) array, Reverse (n-r) array
+	 * 		     Step 3: Reverse the above
+	 * 		     Rundown:
+	 * 		     Eg: 1, 2, 3, 4, 5, 6, 7  n=7, r=3
+	 * 		     To rotate this thrice 1, 2, 3, 4, 5, 6, 7
+	 * 		     1st rotation: 2, 3, 4, 5, 6, 7, 1
+	 * 		     2nd rotation: 3, 4, 5, 6, 7, 1, 2
+	 * 		     3rd rotation: 4, 5, 6, 7, 1, 2, 3
+	 * 		     Reverse (3) array, reverse (7-3=4) array
+	 * 		     (1, 2, 3) , (4, 5, 6, 7)
+	 * 		     (3, 2, 1), (7, 6, 5, 4)
+	 * 		     Reverse the above
+	 * 		     4, 5, 6, 7, 1, 2, 3
+	 * Time Complexity: O(N)
+	 * Space Complexity: O(1) in place
+	 * @param arr int[]
+	 * @param times int
+	 */
+	public static void rotateArray_antiClockwise(int[] arr, int times) {
+		times = times % arr.length;
+		if (times == 0 || arr.length == 1) {
+			return;
+		}
+		reverseArray(arr, 0, times-1);
+		reverseArray(arr, times, arr.length-1);
+		reverseArray(arr, 0, arr.length-1);
+	}
 }
